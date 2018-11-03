d708765c833618af184610f5ab3ae09983f5047a
Mehedi <mehedi.peace@yahoo.com>
2018-11-03 22:33:41 +0000

    Assignment

---
+++ add.js
@@ -0,0 +1,21 @@
+'use strict'
+const cartDOM = document.querySelector('.cart');
+const addToCartButtonsDOM = document.querySelectorAll('[data-action="ADD_TO_CART"]');
+addToCartButtonsDOM.forEach(addToCartButtonDOM =>{
+	addToCartButtonDOM.addEventListener('click',() => {
+	const productDom = addToCartButtonDOM.parentNode;
+	const product = {
+	
+	name:productDom.querySelector('.product-name').innerText,
+	price:productDom.querySelector('.product-price').innerText,
+};
+
+      cartDOM.insertAdjacentHTML('beforeend', `
+        <div class="cart-item">
+          
+          <h3 class="class-item-name">${product.name}</h3>
+          <h3 class="class-item-price">${product.price}</h3>
+        </div>
+      `);
+});
+});
